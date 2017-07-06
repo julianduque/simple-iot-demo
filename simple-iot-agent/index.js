27f31b24de6a97506de70c5dbb246019abcbac2d
Julian Duque <julianduquej@gmail.com>
2017-07-06 10:50:01 -0500

    avoid double connection

--- simple-iot-agent/index.js
+++ simple-iot-agent/index.js
@@ -10,45 +10,55 @@ const EventEmitter = require('events')
 const options = {
   name: 'untitled',
   interval: 3000, // Default 30 seconds
-  mqtt: { host: 'mqtt://localhost' }
+  mqtt: {
+    host: 'mqtt://localhost'
+  }
 }
 
 class IotAgent extends EventEmitter {
-  constructor (opts) {
+  constructor(opts) {
     super()
 
     this.options = defaults(opts, options)
+    this.started = false
   }
 
-  start () {
-    const opts = this.options
-    const client = mqtt.connect(opts.mqtt.host)
+  start() {
+    if (!this.started) {
+      const opts = this.options
+      const client = mqtt.connect(opts.mqtt.host)
+      this.started = true
 
-    client.on('connect', () => {
-      const agentId = uuid.v4()
+      client.on('connect', () => {
+        const agentId = uuid.v4()
 
-      this.emit('connected', agentId)
+        this.emit('connected', agentId)
 
-      setInterval(async () => {
-        let message = {
-          agent: {
-            uuid: agentId,
-            name: opts.name,
-            hostname: os.hostname() || 'localhost',
-            pid: process.pid
-          },
-          value: process.memoryUsage()
-        }
+        setInterval(async() => {
+          let message = {
+            agent: {
+              uuid: agentId,
+              name: opts.name,
+              hostname: os.hostname() || 'localhost',
+              pid: process.pid
+            },
+            value: process.memoryUsage()
+          }
 
-        debug('Sending', message)
+          debug('Sending', message)
 
-        client.publish('agent/message', JSON.stringify(message))
-        this.emit('message', message)
-      }, opts.interval)
-    })
+          client.publish('agent/message', JSON.stringify(message))
+          this.emit('message', message)
+        }, opts.interval)
+      })
+
+      client.on('error', () => {
+        this.started = false
+      })
+    }
   }
 }
 
-module.exports = function createAgent (opts) {
+module.exports = function createAgent(opts) {
   return new IotAgent(opts)
 }
